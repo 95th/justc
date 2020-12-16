24bd0bf34fe71cb3dbe71e2ea06ec25d7584f9d0
Gurwinder Singh <vargwin@gmail.com>
2020-12-16 13:11:18 +0530

    more tests

--- tests/test.rs
+++ tests/test.rs
@@ -78,3 +78,48 @@ fn loop_break_return_in_fn() {
     );
 }
 
+#[test]
+fn struct_expr_and_field_access() {
+    run_ok(
+        r#"
+        struct X { a: int }
+        let x = X { a: 1 };
+        let a: int = x.a;
+    "#,
+    );
+}
+
+#[test]
+fn tuple_struct_expr_and_field_access() {
+    run_ok(
+        r#"
+        struct X(int);
+        let x = X(1);
+        let a: int = x.0;
+    "#,
+    );
+}
+
+#[test]
+fn tuple_return_and_access() {
+    run_ok(
+        r#"
+        fn foo() -> (int, float) {
+            (1, 2.0)
+        }
+        let a: (int, float) = foo();
+        let x: int = foo().0;
+        let x: float = foo().1;
+    "#,
+    );
+}
+
+#[test]
+fn closure_expr_and_call() {
+    run_ok(
+        r#"
+        let f = || (1, 2.0);
+        let a: (int, float) = f();
+    "#,
+    );
+}
